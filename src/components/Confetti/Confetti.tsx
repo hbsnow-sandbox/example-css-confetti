5f8e92a78aaee2fc2441958b20fbda19f9f17ca1
hbsnow <yuki.4uing@gmail.com>
2021-10-11 00:52:01 +0900

    feat: 紙吹雪の落下パターンを増やす

--- src/components/Confetti/Confetti.tsx
+++ src/components/Confetti/Confetti.tsx
@@ -10,30 +10,37 @@ type Props = {
   delay: [number, number];
 };
 
+const classNameList = [style.fallA, style.fallB, style.fallC];
+
 export const Confetti = (props: Props) => {
   const { count, duration, delay } = props;
 
-  const pieceStyles = useMemo(() => {
-    return [...Array(count)].map(() => {
-      const pieceStyle = {
-        marginLeft: `${Math.random() * 100}%`,
-        animationDelay: `-${getRandomArbitrary(...delay)}s`,
-        animationDuration: `${getRandomArbitrary(...duration)}s`,
-      };
+  const pieces = useMemo(() => {
+    return (
+      <>
+        {[...Array(count)].map((_, i) => {
+          const pieceStyle = {
+            marginLeft: `${Math.random() * 100}%`,
+            animationDelay: `-${getRandomArbitrary(...delay)}s`,
+            animationDuration: `${getRandomArbitrary(...duration)}s`,
+          };
 
-      return pieceStyle;
-    });
+          const className =
+            classNameList[Math.floor(Math.random() * classNameList.length)];
+
+          return (
+            <div
+              key={i}
+              className={`${style.piece} ${className}`}
+              style={pieceStyle}
+            >
+              <Piece color="green" width={10} height={10} />
+            </div>
+          );
+        })}
+      </>
+    );
   }, [count, delay, duration]);
 
-  return (
-    <div className={style.root}>
-      {pieceStyles.map((pieceStyle, i) => {
-        return (
-          <div key={i} className={style.piece} style={pieceStyle}>
-            <Piece color="green" width={10} height={10} />
-          </div>
-        );
-      })}
-    </div>
-  );
+  return <div className={style.root}>{pieces}</div>;
 };
